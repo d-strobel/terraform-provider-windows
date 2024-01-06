7c788858660fac0e3ea7aebc20312c0470e370ad
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-01-06 17:24:17 +0100

    chore: initial provider change

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package main
 
 import (
@@ -8,8 +5,9 @@ import (
 	"flag"
 	"log"
 
+	"terraform-provider-windows/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
@@ -38,8 +36,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "registry.terraform.io/d-strobel/windows",
 		Debug:   debug,
 	}
 
