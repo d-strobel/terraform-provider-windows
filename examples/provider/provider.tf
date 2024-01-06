89ff462176d40265cd649a00127fd80945d0fd6f
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-01-06 19:54:21 +0100

    docs: update provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,10 @@
-provider "scaffolding" {
-  # example configuration here
+// Example with a local vagrant machine via WinRM
+provider "windows" {
+  endpoint = "127.0.0.1"
+
+  winrm = {
+    username = "vagrant"
+    password = "vagrant"
+    port     = 15985
+  }
 }
