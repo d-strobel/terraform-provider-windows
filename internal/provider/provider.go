8cfec8e0d259f15a8ae1378d1b2c6c6ed8c394e9
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-01-06 19:38:50 +0100

    refactor: schema generation into different packages

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,84 +1,140 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package provider
 
 import (
 	"context"
-	"net/http"
+	"terraform-provider-windows/internal/generator/provider_windows"
+	"terraform-provider-windows/internal/provider/local"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/d-strobel/gowindows"
+	"github.com/d-strobel/gowindows/connection"
 )
 
-// Ensure ScaffoldingProvider satisfies various provider interfaces.
-var _ provider.Provider = &ScaffoldingProvider{}
+// Ensure WindowsProvider satisfies various provider interfaces.
+var _ provider.Provider = &WindowsProvider{}
 
-// ScaffoldingProvider defines the provider implementation.
-type ScaffoldingProvider struct {
-	// version is set to the provider version on release, "dev" when the
-	// provider is built and ran locally, and "test" when running acceptance
-	// testing.
+// WindowsProvider defines the provider implementation.
+type WindowsProvider struct {
 	version string
 }
 
-// ScaffoldingProviderModel describes the provider data model.
-type ScaffoldingProviderModel struct {
-	Endpoint types.String `tfsdk:"endpoint"`
-}
-
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+func (p *WindowsProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "windows"
 	resp.Version = p.version
 }
 
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
-				Optional:            true,
-			},
-		},
-	}
+func (p *WindowsProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	// Use generated schema
+	resp.Schema = provider_windows.WindowsProviderSchema(ctx)
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+func (p *WindowsProvider) ConfigValidators(ctx context.Context) []provider.ConfigValidator {
+	return []provider.ConfigValidator{}
+}
+
+func (p *WindowsProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data provider_windows.WindowsModel
 
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 
+	// Check for nested schema
+	if data.Winrm.IsNull() && data.Ssh.IsNull() {
+		resp.Diagnostics.AddError("Invalid provider configuration", "ssh or winrm must be specified")
+	}
+	if !data.Winrm.IsNull() && !data.Ssh.IsNull() {
+		resp.Diagnostics.AddError("Invalid provider configuration", "only one of ssh or winrm must be specified")
+	}
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	// Init connection configuration
+	config := &connection.Config{}
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
+	// Check and setup WinRM configuration
+	if !data.Winrm.IsNull() {
+
+		// Check required parameters for WinRM
+		if data.Winrm.Username.IsNull() {
+			resp.Diagnostics.AddAttributeError(path.Root("winrm"), "Missing parameter for winrm schema", "Parameter 'username' must be set")
+		}
+		if data.Winrm.Password.IsNull() {
+			resp.Diagnostics.AddAttributeError(path.Root("winrm"), "Missing parameter for winrm schema", "Parameter 'password' must be set")
+		}
+		if resp.Diagnostics.HasError() {
+			return
+		}
+
+		// Init WinRM config
+		config.WinRM = &connection.WinRMConfig{}
+
+		config.WinRM.WinRMHost = data.Endpoint.ValueString()
+		config.WinRM.WinRMUsername = data.Winrm.Username.ValueString()
+		config.WinRM.WinRMPassword = data.Winrm.Password.ValueString()
+
+		if !data.Winrm.Port.IsNull() {
+			config.WinRM.WinRMPort = int(data.Winrm.Port.ValueInt64())
+		}
+		if !data.Winrm.Timeout.IsNull() {
+			config.WinRM.WinRMTimeout = time.Duration(data.Winrm.Timeout.ValueInt64())
+		}
+		if !data.Winrm.Port.IsNull() {
+			config.WinRM.WinRMPort = int(data.Winrm.Port.ValueInt64())
+		}
+	}
+
+	// Check and set SSH configuration
+	if !data.Ssh.IsNull() {
+
+		// Check required parameters for SSH
+		if data.Ssh.Username.IsNull() {
+			resp.Diagnostics.AddAttributeError(path.Root("ssh"), "Missing parameter for ssh schema", "Parameter 'username' must be set")
+		}
+		if data.Ssh.Password.IsNull() {
+			resp.Diagnostics.AddAttributeError(path.Root("ssh"), "Missing parameter for ssh schema", "Parameter 'password' must be set")
+		}
+		if resp.Diagnostics.HasError() {
+			return
+		}
+		// Init SSH config
+		config.SSH = &connection.SSHConfig{}
+
+		config.SSH.SSHHost = data.Endpoint.ValueString()
+		config.SSH.SSHUsername = data.Ssh.Username.ValueString()
+	}
+
+	// Setup client
+	client, err := gowindows.NewClient(config)
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to setup client", err.Error())
+		return
+	}
+
 	resp.DataSourceData = client
 	resp.ResourceData = client
 }
 
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{
-		NewExampleResource,
-	}
+func (p *WindowsProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return nil
+	// return []func() resource.Resource{
+	// 	NewExampleResource,
+	// }
 }
 
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+func (p *WindowsProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
-		NewExampleDataSource,
+		local.NewLocalGroupDataSource,
 	}
 }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
-		return &ScaffoldingProvider{
+		return &WindowsProvider{
 			version: version,
 		}
 	}
