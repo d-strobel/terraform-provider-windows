43b369da6e97b1dada114fa969cd78816c396cdc
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-01-12 14:06:03 +0100

    chore: update local group datasource with scaffold command

--- internal/provider/local/local_group_data_source.go
+++ internal/provider/local/local_group_data_source.go
@@ -11,36 +11,32 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Ensure provider defined types fully satisfy framework interfaces.
-var _ datasource.DataSource = &LocalGroupDataSource{}
+var _ datasource.DataSource = (*localGroupDataSource)(nil)
 
 func NewLocalGroupDataSource() datasource.DataSource {
-	return &LocalGroupDataSource{}
+	return &localGroupDataSource{}
 }
 
-// LocalGroupDataSource defines the data source implementation.
-type LocalGroupDataSource struct {
+type localGroupDataSource struct {
 	client *gowindows.Client
 }
 
-func (d *LocalGroupDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_local" + "_group"
+func (d *localGroupDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_local_group"
 }
 
-func (d *LocalGroupDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+func (d *localGroupDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = local_datasources.LocalGroupDataSourceSchema(ctx)
 	resp.Schema.Description = `Retrieve information about a local security group.
 You can get a group by the name or the security ID of the group.
 `
 }
 
-func (d *LocalGroupDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
-	// Prevent panic if the provider has not been configured.
+func (d *localGroupDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
 
-	// Check data source client type
 	client, ok := req.ProviderData.(*gowindows.Client)
 	if !ok {
 		resp.Diagnostics.AddError(
@@ -53,26 +49,27 @@ func (d *LocalGroupDataSource) Configure(ctx context.Context, req datasource.Con
 	d.client = client
 }
 
-func (d *LocalGroupDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+func (d *localGroupDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
 	var data local_datasources.LocalGroupModel
 
 	// Read Terraform configuration data into the model
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Group params
+	// Read API call logic
 	params := local.GroupParams{
 		Name: data.Name.ValueString(),
 		SID:  data.Sid.ValueString(),
 	}
 
-	// Client call
 	winResp, err := d.client.Local.GroupRead(ctx, params)
 	if err != nil {
 		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read local security group, got error: %s", err))
 		return
+
 	}
 
 	// Save data into Terraform state
