ed437b8453495c75750c5149ace233677350657b
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-05-06 23:26:33 +0200

    docs: add attribute to local user resource example

--- examples/resources/windows_local_user/resource.tf
+++ examples/resources/windows_local_user/resource.tf
@@ -3,6 +3,7 @@ resource "windows_local_user" "this" {
   full_name                = "My User"
   description              = "This is a test user"
   password                 = "P@ssw0rd!"
+  account_expires          = "2023-10-30T18:00:00Z"
   enabled                  = true
   password_never_expires   = true
   user_may_change_password = true
